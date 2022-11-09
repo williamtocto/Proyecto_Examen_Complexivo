6aacce7967f9138dcad088197ad3ec8064b9274d
Aronfs <aronviracocha7@gmail.com>
2022-11-09 18:08:11 -0500

    Hice la ventanas de facturacion de los productos que el usuario elegira

---
+++ app/src/main/java/com/example/proyecto_examen_complexivo/facturacion.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_examen_complexivo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class facturacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facturacion);
+    }
+}
