74b2f67904622772fae2505046666084efa2f06c
Sbryan20 <91134367+Sbryan20@users.noreply.github.com>
2022-11-09 15:19:59 -0500

    Creacion menu

--- app/src/main/java/com/example/proyecto_examen_complexivo/MainActivity.java
+++ app/src/main/java/com/example/proyecto_examen_complexivo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.proyecto_examen_complexivo;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ingresa = (Button) findViewById(R.id.btn_ingresar20);
+        ingresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Navegacion.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
