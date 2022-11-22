c35f16e91b4f6eca941b710b8d5fb09e77a98ba4
Sbryan20 <91134367+Sbryan20@users.noreply.github.com>
2022-11-22 18:49:09 -0500

    Filtro completo0

--- app/src/main/java/com/example/proyecto_examen_complexivo/Fragments/detalle_compras.java
+++ app/src/main/java/com/example/proyecto_examen_complexivo/Fragments/detalle_compras.java
@@ -27,6 +27,7 @@ import java.util.List;
  */
 public class detalle_compras extends Fragment {
 
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
