a936089dd50f4de769f5b050548bce449fed1d61
Aronfs <aronviracocha7@gmail.com>
2022-11-27 01:10:07 -0500

    Solo falta a actualizar el fragment de detalle compras cuando se realiza una modificacion lo demas ya esta hecho

--- app/src/main/java/com/example/proyecto_examen_complexivo/EditarProductoDetalle.java
+++ app/src/main/java/com/example/proyecto_examen_complexivo/EditarProductoDetalle.java
@@ -40,6 +40,30 @@ public class EditarProductoDetalle extends AppCompatActivity {
         btnActualizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                txtCantidad.setError(null);
+                String posibleNumero = txtCantidad.getText().toString();
+                // Vemos si está vacío...
+                // Notación yoda: https://parzibyte.me/blog/2018/09/24/notacion-yoda-en-programacion/
+                if ("".equals(posibleNumero)) {
+                    // Primer error
+                    txtCantidad.setError("Introduce un número");
+                    // Le damos focus
+                    txtCantidad.requestFocus();
+                    // Y terminamos la ejecución
+                    return;
+                }
+                // En caso de que hayan puesto algo, convertimos a entero
+                int numero = Integer.parseInt(posibleNumero);
+                // Comparar si está en el rango
+                if (numero >= 5 && numero <= 13) {
+                    // La validación termina y hacemos lo que vayamos a hacer
+                    Toast.makeText(EditarProductoDetalle.this, "Todo correcto", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Si no, entonces indicamos el error y damos focus
+                    txtCantidad.setError("Número fuera de rango");
+                    txtCantidad.requestFocus();
+                }
                 Carrito carritoActual = new Carrito();
                 producto = (Carrito) getIntent().getExtras().getSerializable("productodetalle");
                 Toast.makeText(EditarProductoDetalle.this, ""+producto.getId_producto(), Toast.LENGTH_SHORT).show();
@@ -48,6 +72,7 @@ public class EditarProductoDetalle extends AppCompatActivity {
                 carritoActual.ActualizarCarrito(EditarProductoDetalle.this, producto.getId_producto());
                 finish();
             }
+
         });
     }
 
